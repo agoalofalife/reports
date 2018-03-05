f9d598cdbf761d5de5f51f32ef4d517c56450787
agoalofalife <agoalofalife@gmail.com>
2018-03-05 17:56:30 +0300

    build ui

--- resources/assets/js/pages/Table-Reports.vue
+++ resources/assets/js/pages/Table-Reports.vue
@@ -1,7 +1,72 @@
 <template>
 <div>
-    <el-row class="table-reports">
-        <div>Hello потомки!</div>
+    <el-row>
+        <!--<el-radio-group v-model="states.emptyNotification" style="margin-bottom: 20px;">-->
+            <!--<el-radio-button :label="false">expand</el-radio-button>-->
+            <!--<el-radio-button :label="true">collapse</el-radio-button>-->
+        <!--</el-radio-group>-->
+        <!--<el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="states.emptyNotification">-->
+            <!--<el-menu-item index="2">-->
+                <!--<i class="el-icon-menu"></i>-->
+                <!--<span slot="title">Navigator Two</span>-->
+            <!--</el-menu-item>-->
+            <!--<el-menu-item index="3" disabled>-->
+                <!--<i class="el-icon-document"></i>-->
+                <!--<span slot="title">Navigator Three</span>-->
+            <!--</el-menu-item>-->
+            <!--<el-menu-item index="4">-->
+                <!--<i class="el-icon-setting"></i>-->
+                <!--<span slot="title">Navigator Four</span>-->
+            <!--</el-menu-item>-->
+        <!--</el-menu>-->
+        <el-badge :value="1" :max="1000" class="item">
+            <i class="fa fa-bell" aria-hidden="true"></i>
+        </el-badge>
+        <el-table
+                :data="tableData"
+                style="width: 100%"
+                height="650"
+        >
+            <el-table-column
+                    fixed
+                    prop="name"
+                    label="Name"
+                    width="150">
+            </el-table-column>
+            <el-table-column
+                    prop="description"
+                    label="Description"
+                    width="250">
+            </el-table-column>
+            <el-table-column
+                    prop="date"
+                    label="Date last update"
+                    width="250">
+            </el-table-column>
+            <el-table-column
+                    fixed="right"
+                    label="Operations"
+                    width="120">
+                <template slot-scope="scope">
+                    <i class="fa fa-arrow-circle-down"
+                       aria-hidden="true"
+                       v-show="states.error === false && states.process === false"
+                    ></i>
+                    <i class="fa fa-exclamation-circle"
+                       aria-hidden="true"
+                       v-show="states.error"></i>
+                    <i class="fa fa-refresh"
+                       aria-hidden="true"
+                       @click="handleClickDownload"
+                       v-show="states.error === false && states.process === false"
+                    ></i>
+                    <i class="el-icon-loading" aria-hidden="true" v-show="states.process"></i>
+                    <!--<i class="fa fa-file-excel-o" aria-hidden="true"></i>-->
+                    <!--<el-button type="text" size="small"><i class="el-icon-download"></i></el-button>-->
+                    <!--<el-button type="text" size="small">Download</el-button>-->
+                </template>
+            </el-table-column>
+        </el-table>
     </el-row>
 </div>
 </template>
@@ -12,7 +77,27 @@
     export default {
         data(){
             return {
-
+                states:{
+                  process:false,
+                  error:false,
+                  emptyNotification:false,
+                },
+                tableData: [{
+                    name: 'Report Seller',
+                    description: 'Report for sale seller',
+                    date: '2018-01-23 11:43:13',
+                }]
+            }
+        },
+        methods :{
+            handleClickDownload(){
+                this.states.process = true;
+            },
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
             }
         }
     }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,5 +2,5 @@
     <div id="root"></div>
     <script src="{{ mix('js/app.js', 'vendor/reports') }}"></script>
 </div>
-{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/element-ui/1.4.8/theme-default/index.css">--}}
-{{--<link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">--}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/element-ui/1.4.8/theme-default/index.css">
+<link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
