4ad283e84a90b4e0db4e02b720a81da9ac5a23b9
agoalofalife <agoalofalife@gmail.com>
2018-03-06 09:41:45 +0300

    create migration reports and create Model Report

---
+++ database/migrations/2018_03_06_062750_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('class_name')->unique()->index();
+            $table->enum('status', ['error', 'process', 'completed']);
+            $table->boolean('is_completed')->comment('Check read or not');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
---
+++ src/Models/Report.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace agoalofalife\Reports\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+
+}
