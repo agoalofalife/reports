666296940b5cbd46f7c40f0a64759ca2465dc03f
Ilya <agoalofalife@gmail.com>
2018-06-07 22:56:25 +0400

    fix bag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,4 +3,4 @@
     <script src="{{ mix('js/app.js', 'vendor/reports') }}"></script>
 </div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/element-ui/1.4.8/theme-default/index.css">
-<link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
+<script src="https://use.fontawesome.com/586d7e8b5c.js"></script>
