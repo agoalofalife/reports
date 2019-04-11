ded7a49d8a793fdfd640f0ccb8f452a5acae6df2
Ilya <agoalofalife@gmail.com>
2019-04-11 10:26:55 +0300

    Add option path to js file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div id="root"></div>
-    <script src="{{ mix('js/app.js', 'vendor/reports') }}"></script>
+    <script src="{{ mix('js/app.js', config('reports.path_js')) }}"></script>
 </div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/element-ui/1.4.8/theme-default/index.css">
 <script src="https://use.fontawesome.com/586d7e8b5c.js"></script>
